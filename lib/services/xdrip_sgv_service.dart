7b01db00c218304e731d735ae817a288c6cadc88
Levi Tomes <levi@userdevice.net>
2025-01-16 08:52:10 -0600

    Update xdrip sgv service to send latest glucose value to headset as text each time fetchSgvData is invoked.

--- lib/services/xdrip_sgv_service.dart
+++ lib/services/xdrip_sgv_service.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:fahrplan/models/android/xdrip_sgv_model.dart';
+import 'package:fahrplan/services/bluetooth_manager.dart';
 
 class SgvService {
   Future<List<SgvResponse>> fetchSgvData() async {
     const String uri = "http://127.0.0.1:17580/sgv.json";
-
+    final bt = BluetoothManager();
     final response = await http.get(Uri.parse(uri));
     print('response.body');
     print(response.body);
@@ -13,6 +14,9 @@ class SgvService {
       List jsonResponse = jsonDecode(response.body) as List;
       print('Number of entries: ${jsonResponse.length}');
       print(jsonResponse);
+      List<SgvResponse> sgvData = jsonResponse.map((e) => SgvResponse.fromJson(e)).toList();
+
+      bt.sendText(sgvData.first.sgv.toString());
       return jsonResponse.map((e) => SgvResponse.fromJson(e)).toList();
     } else {
       throw Exception("Failed to load data");
